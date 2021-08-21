f5a695f21549c5cfba2676861319481dc198eca8
piashmahmud <61230770+piashmahmud@users.noreply.github.com>
2021-08-22 01:24:05 +0600

    Add files via upload
    added link list

--- Main.c
+++ Main.c
@@ -1,8 +1,20 @@
+
 #include<stdio.h>
+#include <stdlib.h>
 
 #define MAX_LEN 1000
 
 int main(){
+struct Node {
+    int index;
+    char type[20];
+    struct Node* next;
+    struct Node* previous;
+};
+int dividingChars=0;
+char inputFileString[MAX_LEN+1];
+
+void fileReader(){
     int i,ch;
     char inputFileString[MAX_LEN+1];
 
@@ -11,6 +23,18 @@ int main(){
       inputFileString[i] = ch;
     inputFileString[i] = '\0';
     fclose(inputFile);
+}
+
+void printList(struct Node* n)
+{
+    while (n != NULL) {
+        printf(" %d. %s", n->index,n->type);
+        n = n->next;
+    }
+}
+
+int main(){
+    fileReader();
 
     printf("%s",inputFileString);
     return 0;
